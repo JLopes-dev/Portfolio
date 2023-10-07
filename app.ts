54606f08ca54983f9091bd0e475026026955dc2b
JotaPe8 <joaopedrolopessardinha@gmail.com>
2023-10-07 16:01:46 -0300

    dotenv

--- app.ts
+++ app.ts
@@ -2,8 +2,8 @@ import mongo from './database/Mongo'
 import routesHandler from './routes/Routes'
 import { config } from 'dotenv'
 
-if(process.env.DB === 'dev') config({path: './config/.env.dev'})
-if(process.env.DB === 'prod') config({path:'./config/.env.prod'})
+if(process.env.DB_DEV === 'dev') config({path: './config/.env.dev'})
+if(process.env.DB_PROD === 'prod') config({path:'./config/.env.prod'})
 
 mongo.mongoConnect(process.env.DB as string).then(() => {
     routesHandler.start()
