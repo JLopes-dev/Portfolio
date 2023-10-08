da350edf1cffd1f7fd0ad6065f751b974ac56ba1
JotaPe8 <joaopedrolopessardinha@gmail.com>
2023-10-08 15:41:56 -0300

    Separação por pastas

--- CrudMVC/app.ts
+++ CrudMVC/app.ts
@@ -5,6 +5,7 @@ import { config } from 'dotenv'
 if(process.env.DB_DEV === 'dev') config({path: './config/.env.dev'})
 if(process.env.DB_PROD === 'prod') config({path:'./config/.env.prod'})
 
-mongo.mongoConnect(process.env.DB as string).then(() => {
+mongo.mongoConnect().then(() => {
     routesHandler.start()
 })
+// process.env.DB as string
--- CrudMVC/database/Mongo.ts
+++ CrudMVC/database/Mongo.ts
@@ -1,10 +1,12 @@
 import mongoose from "mongoose";
 
 class Mongo {
-    public async mongoConnect(DBurl: string) {
-        await mongoose.connect(DBurl)
+    public async mongoConnect() {
+        await mongoose.connect('mongodb://127.0.0.1:27017/Portfolio')
         .then(() => console.log('mongoose...'));
     } 
 }
 const mongo = new Mongo()
 export default mongo
+//DBurl: string
+//DBurl
