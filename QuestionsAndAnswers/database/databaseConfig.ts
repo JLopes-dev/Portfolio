6e1544cf75a7f031a51b7ee603a387611f0ff69f
JotaPe Lopes <118218289+JotaPe8@users.noreply.github.com>
2023-11-17 13:16:46 -0300

    Delete QuestionsAndAnswers directory

--- QuestionsAndAnswers/database/databaseConfig.ts
+++
@@ -1,6 +0,0 @@
-import { Sequelize } from 'sequelize'
-const sequelize = new Sequelize('QuestionsAndAnswers', 'root', '@Jplsmvmv88', {
-    host: '127.0.0.1',
-    dialect: 'mysql'
-})
-export default sequelize;
--- QuestionsAndAnswers/index.ts
+++
@@ -1,7 +0,0 @@
-import { config } from 'dotenv';
-import routes from './routes/Routes';
-
-if(process.env.CONFIG === 'prod') config({ path: './config/.env.prod' })
-if(process.env.CONFIG === 'dev') config({ path: './config/.env.prod' })
-
-routes.start()
